a4d5436cb7da906b46f5c73d3ffa1f23a928a80b
wep1980 <wepbike@gmail.com>
2021-06-15 22:51:36 -0300

    Utilizando o extended entity Manager

--- src/main/java/br/com/wepdev/casadocodigo/DAO/LivroDAO.java
+++ src/main/java/br/com/wepdev/casadocodigo/DAO/LivroDAO.java
@@ -3,14 +3,17 @@ package br.com.wepdev.casadocodigo.DAO;
 
 import br.com.wepdev.casadocodigo.models.Livro;
 
+import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import java.util.List;
 
+@Stateful // E necessario ao utilizar o EXTENDED quando se ultiliza no @PersistenceContext(type = PersistenceContextType.EXTENDED) (EJB)
 public class LivroDAO {
 
-
-    @PersistenceContext
+    // O tempo de vida do entityManager dura não so durante uma transação, mas durante o escopo. mas utilizada para evitar querys complexas
+    @PersistenceContext(type = PersistenceContextType.EXTENDED) // SOLUÇÃO 1
     private EntityManager entityManager;
 
 
@@ -41,11 +44,23 @@ public class LivroDAO {
     public List<Livro> demaisLivros() {
         String jpql = "select l from Livro l order by l.dataPublicacao desc "; // Ordena pela data de publicacao de maneira decrescente
         return entityManager.createQuery(jpql , Livro.class)
-                .setFirstResult(6) // O primeiro resultado começa a partir do sexto livro
+                .setFirstResult(5) // O primeiro resultado começa a partir do sexto livro
                 .getResultList();
     }
 
+    /**
+     * JOIN FETCH -> FAZ A BUSCA , A RELAÇÃO COM OS AUTORES LIGADOS AO LIVRO
+     * @param id
+     * @return
+     */
     public Livro buscarPorId(Integer id) {
-        return entityManager.find(Livro.class , id);
+        // SOLUÇÃO 1 -> Acontecia um erro apos buscar os ids, pq a conexão se encerrava e n buscava os autores
+        return entityManager.find(Livro.class, id);
+        
+        // SOLUÇÃO 2 -> para querys menos complexas, solucão mais performatica
+        //String jpql = "select l from Livro l join fetch l.autores where l.id = :id";
+        //return entityManager.createQuery(jpql , Livro.class)
+               // .setParameter("id" , id)
+               // .getSingleResult(); // Busca o id e apenas um resultado que é o livro
     }
 }
